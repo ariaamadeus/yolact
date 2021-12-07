034b6bcaf03ffbce8964dd59cec4cccfbaa21d29
Aria Amadeus <amadeus.aria@gmail.com>
2021-12-07 17:05:26 +0700

    Master

---
+++ master.py
@@ -0,0 +1,26 @@
+import time
+import os
+import torch
+import json
+
+#from data import cfg, set_cfg, set_dataset
+import socket_server as ss #starServer(HOST, PORT), sendServer("message")
+import eval
+import time
+
+HOST = "192.168.5.71"
+PORT = 7002
+CONFIG = "yolact_resnet50_kelapa_sawit_config"
+MODEL = "weights/yolact_plus_resnet50_kelapa_sawit_77_544_interrupt.pth"
+score_threshold=0.5
+top_k=1
+labels = ['Accept','Reject']
+
+#images="test_images:output_image"
+eval.theInit(MODEL, CONFIG)
+s = ss.startServer(HOST,PORT)
+while True:
+	camFeed = ss.recvData(s)
+	result = eval.outEval(score_threshold, top_k,camFeed)
+	ss.FBData(json.dumps({"classes":labels[(result[0][0].tolist())[0]],"scores":(result[1][0].tolist())[0]}))
+	#ss.FBData(json.dumps({"classes":labels[(result[0][0].tolist())[0]],"scores":result[1][0].tolist(),"boxes":result[2][0].tolist()}))
